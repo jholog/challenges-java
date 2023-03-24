19795db48d1590d55944f93acd5363a4e717b631
jholog <wnsgh0304@gmail.com>
2023-03-24 18:45:40 +0900

    Improve the existing code
    - class 명 수정
    - level1 코드 보완
    - level2 코드 보완

--- src/programmers/skillchecks/Level1.java
+++ src/programmers/skillchecks/Level1.java
@@ -1,12 +1,12 @@
 package programmers.skillchecks;
 
-public class Level1_345109 {
+import java.util.Arrays;
+
+public class Level1 {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = {};
 
-        // check arr length, 행과 열의 크기가 같다고 가정
-        if (arr1.length > 500 || arr1[0].length > 500) return answer;
-
+        // check arr length, 행과 열의 크기는 같다고 가정
         answer = new int[arr1.length][arr1[0].length];
 
         for (int i = 0; i < arr1.length; i++) {
@@ -21,8 +21,8 @@ public class Level1_345109 {
         int[][] arr1 = {{1,2},{2,3}};
         int[][] arr2 = {{3,4},{5,6}};
 
-        Level1_345109 l = new Level1_345109();
+        Level1 l = new Level1();
         int[][] answer = l.solution(arr1, arr2);
-        System.out.println(answer);
+        System.out.println(Arrays.deepToString(answer));
     }
 }
--- src/programmers/skillchecks/Level1_2.java
+++ src/programmers/skillchecks/Level1_2.java
@@ -1,11 +1,12 @@
 package programmers.skillchecks;
 
-public class Level1_345109_2 {
+public class Level1_2 {
     public String solution(int a, int b) {
         String answer = "";
 
-        String[] day = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
-        int[] month_val = {31,28,31,30,31,30,31,31,30,31,30,31 };
+        // 2016년 1월 1일은 금요일
+        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] month_val = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int year = 2016;
 
         // check leap year
@@ -13,14 +14,11 @@ public class Level1_345109_2 {
             month_val[1] = 29;
 
         int total = 0;
-
         for (int i = 0; i < (a - 1); i++)
             total += month_val[i];
-        total += b - 1;     // day string array 의 index 값에 접근하기 위해 -1
+        total = (total + b) % 7;
 
-        int answer_val = (5 + total) % 7;
-        answer = day[answer_val];
-
+        answer = day[total - 1];
         return answer;
     }
 
@@ -28,7 +26,7 @@ public class Level1_345109_2 {
         int a = 5;
         int b = 24;
 
-        Level1_345109_2 l = new Level1_345109_2();
+        Level1_2 l = new Level1_2();
         System.out.println(l.solution(a, b));
     }
 }
