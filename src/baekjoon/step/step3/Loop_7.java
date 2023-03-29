be1af4b1c38c23229cbe141b419988c63e9e7432
jholog <wnsgh0304@gmail.com>
2023-03-29 10:09:51 +0900

    baekjoon loop solved (5 to 10)

---
+++ src/baekjoon/step/step3/Loop_7.java
@@ -0,0 +1,16 @@
+package baekjoon.step.step3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Loop_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] line = sc.nextLine().split(" ");
+            int val = Arrays.stream(Arrays.stream(line).mapToInt(Integer::parseInt).toArray()).sum();
+            System.out.println("Case #" + (i + 1) + ": " + val);
+        }
+    }
+}
